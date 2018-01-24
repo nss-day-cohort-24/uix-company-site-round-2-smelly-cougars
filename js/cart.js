ac0d1dacdb32c6ae1c6333173b107978e2bae857
lapinell <lapinell@gmail.com>
2018-01-24 10:44:23 -0600

    final changes to cart

--- js/cart.js
+++ js/cart.js
@@ -7,6 +7,12 @@ console.log(price);
 var totalPrice = quantity * price;
 console.log(totalPrice);
 
-document.getElementById("product1total").innerText = totalPrice;
+document.getElementById("product1total").innerText = totalPrice.toFixed(2);
 
 // define recalculate function for button so it reloads the totalPrice with the updated total using the updated quantity
+
+function recalculate () {
+    location.reload();
+}
+
+document.getElementById("grandTotal").innerText = totalPrice.toFixed(2);
