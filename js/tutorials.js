9665d715594aac04411c4a77907eef18f73606b0
Sandhya Rao <smudra@yahoo.com>
2018-01-24 12:59:57 -0600

    made changes to tutorials array

--- js/tutorials.js
+++ js/tutorials.js
@@ -3,65 +3,65 @@ var products = [
     {
         name: 'Jazz Harmony 1',
         duration: '2:00' ,
-        description: ' Remove the worry from your child traveling alone with a tracking app that includes checkpoints, mini-games, and notifications for when your child arrives at their destination.',
-        price: '$0.99',
-        imgURL: '../images/#',
+        description: 'Jazz Harmony for the advanced musician.',
+        price: '$25.00',
+        imgURL: '../images/star-factor-2.png',
         link: '../jazz-harmony-1.html'
     },
     {
         name: 'Jazz Harmony 2',
         duration: '2:00',
-        description: 'A simplified calendar built for parents to keep track of their child\'s wild schedule of daily events.',
-        price: '$0.99',
-        imgURL: '../images/#',
+        description: 'Jazz Harmony for the advanced musician.',
+        price: '$25.00',
+        imgURL: '../images/star-factor-3.png',
         link: '../#'
     },
     {
         name: 'Vocal Warmups 1',
         duration: '2:00',
-        description: 'Be sure your child is in caring hands while traveling to and from home with our trusted group of drivers.',
-        price: '$0.99',
-        imgURL: '../images/#',
+        description: 'Vocal warmups for the advanced musician.',
+        price: '$25.00',
+        imgURL: '../images/star-factor-4.png',
         link: '../#'
     },
     {
         name: 'Vocal Warmups 2',
         duration: '2:00',
-        description: 'Practice honesty with your child with our lie detecting app that can discreetly analyze if your child is telling the truth or a fib.',
-        price: '$0.99',
-        imgURL: '../images/#',
+        description: 'Vocal warmups for the advanced musician.',
+        price: '$25.00',
+        imgURL: '../images/star-factor-5.png',
         link: '../#'
     },
     {
         name: 'Guitar 1',
         duration: '2:00',
-        description: 'Open your child\'s creative side with a simple yet lively dress designer suitable for any and all levels of imagination.',
-        price: '$0.99',
-        imgURL: '../images/#',
+        description: 'Jazz Harmony for the advanced musician.',
+        price: '$25.00',
+        imgURL: '../images/star-factor-6.png',
         link: '../#'
     },
     {
         name: 'Guitar 2',
         duration: '2:00',
-        description: 'Find the Prince Charming that fits your fantasy perfectly.',
-        price: '$0.99',
-        imgURL: '../images/#',
+        description: 'Jazz Harmony for the advanced musician.',
+        price: '$25.00',
+        imgURL: '../images/star-factor-7.png',
         link: '../#'
     },
     {
         name: 'Rhythm Study 1',
         duration: '2:00',
-        description: 'Cookbook app of famous recipes from other stories/fables. Make cooking fun with re-creations of favorite recipes from your child\'s favorite tales and stories.',
-        price: '$6.00',
-        imgURL: '../images/#',
+        description: 'Jazz Harmony for the advanced musician.',
+        price: '$25.00',
+        imgURL: '../images/star-factor-8.png',
         link: '../#'
     },
     {
         name: 'Rhythm Study 2',
         duration: '2:00',
-        description: 'Keep you and your child healthy by monitoring things such as their exercise and nutrition. Keep them involved by staying healthy with them and sharing your progress with each other along the way.',
-        price: '$0.99',
-        imgURL: '../images/#',
+        description: 'Jazz Harmony for the advanced musician.',
+        price: '$25.00',
+        imgURL: '../images/star-factor-1.png',
         link: '../#'
     }
 ];
